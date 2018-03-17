595098ae6b13da010d56a6d04e1b817f98dd48da
Eka Rudianto <ekarudianto@yahoo.com>
2018-03-17 17:29:44 +0800

    Add routing & shorten funcs

--- actions/app.go
+++ actions/app.go
@@ -3,21 +3,15 @@ package actions
 import (
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/buffalo/middleware"
-	"github.com/gobuffalo/buffalo/middleware/ssl"
 	"github.com/gobuffalo/envy"
-	"github.com/unrolled/secure"
 
 	"github.com/buffalo/app/models"
-	"github.com/gobuffalo/buffalo/middleware/csrf"
-	"github.com/gobuffalo/buffalo/middleware/i18n"
-	"github.com/gobuffalo/packr"
 )
 
 // ENV is used to help switch settings based on where the
 // application is being run. Default is "development".
 var ENV = envy.Get("GO_ENV", "development")
 var app *buffalo.App
-var T *i18n.Translator
 
 // App is where all routes and middleware for buffalo
 // should be defined. This is the nerve center of your
@@ -28,35 +22,24 @@ func App() *buffalo.App {
 			Env:         ENV,
 			SessionName: "_app_session",
 		})
-		// Automatically redirect to SSL
-		app.Use(ssl.ForceSSL(secure.Options{
-			SSLRedirect:     ENV == "production",
-			SSLProxyHeaders: map[string]string{"X-Forwarded-Proto": "https"},
-		}))
 
 		if ENV == "development" {
 			app.Use(middleware.ParameterLogger)
 		}
-
-		// Protect against CSRF attacks. https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)
-		// Remove to disable this.
-		app.Use(csrf.New)
 
 		// Wraps each request in a transaction.
 		//  c.Value("tx").(*pop.PopTransaction)
 		// Remove to disable this.
 		app.Use(middleware.PopTransaction(models.DB))
 
-		// Setup and use translations:
-		var err error
-		if T, err = i18n.New(packr.NewBox("../locales"), "en-US"); err != nil {
-			app.Stop(err)
-		}
-		app.Use(T.Middleware())
-
+		// Routing collection
 		app.GET("/", HomeHandler)
+		app.GET("/{encodedURL}", DecodeURL)
 
-		app.ServeFiles("/", assetsBox) // serve files from the public directory
+		app.POST("/shorten", ShortenURL)
+
+		// serve files from the public directory
+		app.ServeFiles("/", assetsBox)
 	}
 
 	return app
--- actions/home.go
+++ actions/home.go
@@ -1,9 +1,49 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"github.com/buffalo/app/models"
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/pop"
+)
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
 	return c.Render(200, r.HTML("index.html"))
+}
+
+// Decode url
+func DecodeURL(c buffalo.Context) error {
+	return c.Render(200, r.JSON(map[string]string{"version": "v1.0.0"}))
+}
+
+// Short url
+func ShortenURL(c buffalo.Context) error {
+	url := c.Request().FormValue("url")
+
+	// Connect to DB
+	tx, ok := c.Value("tx").(*pop.Connection)
+
+	if !ok {
+		return c.Render(500, r.JSON(map[string]string{"error": "Database connection error"}))
+	}
+
+	link := &models.Link{}
+
+	// Find existing record and if fails
+	// Create one registry
+	if err := tx.Where("links.link = ?", url).First(link); err != nil {
+		link.Link = url
+		link.ShortLink = link.RandSeq(tx, 6)
+
+		verrs, e := tx.ValidateAndCreate(link)
+
+		if e != nil || verrs.HasAny() {
+			return c.Render(500, r.JSON(map[string]string{"error": "Error inserting record"}))
+		}
+
+		return c.Render(200, r.JSON(link))
+	}
+
+	return c.Render(200, r.JSON(link))
 }
