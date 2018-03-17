aa14747d51d3360e9209d4bc80a46a8f5e2e9cd4
Eka Rudianto <ekarudianto@yahoo.com>
2018-03-17 17:33:33 +0800

    Remove generated lines

--- models/link.go
+++ models/link.go
@@ -23,15 +23,6 @@ func (l Link) String() string {
 	return string(jl)
 }
 
-// Links is not required by pop and may be deleted
-type Links []Link
-
-// String is not required by pop and may be deleted
-func (l Links) String() string {
-	jl, _ := json.Marshal(l)
-	return string(jl)
-}
-
 // Method to generate random slugs
 func (l *Link) RandSeq(tx *pop.Connection, n int) string {
 	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789$-_.+!*'()")
